a9dd6224d8f41100383453080384aa3062ba39de
EngrBassey <basseyleads001>
2023-08-09 13:54:13 -0700

    print last digit

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    new_char = str[:n] + str[n+1:]
-    return (new_char)
+#if n >= 0:
+    if 0 <= n < len(str):
+        new_char = str[:n] + str[n+1:]
+        return (new_char)
+    else n < 0:
+        return (str)
