26fc0024d7b501fbca549648aadd96416c1a9cf6
EngrBassey <basseyleads001>
2023-08-09 14:01:49 -0700

    slicing elements

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def remove_char_at(str, n):
-#if n >= 0:
-    if 0 <= n < len(str):
-        new_char = str[:n] + str[n+1:]
-        return (new_char)
-    else n < 0:
-        return (str)
+def remove_char_at(s, n):
+    new_str = ""
+    for i in range(len(s)):
+        if i != n:
+            new_str += s[i]
+    return new_str
