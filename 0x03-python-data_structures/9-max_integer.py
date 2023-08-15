474772ee9fd972cb5fb9564b35090d4fd693733c
EngrBassey <basseyleads001>
2023-08-15 01:56:20 -0700

    pg thats check for the highest value

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    else:
+        max_val = my_list[0]
+        for j in range(len(my_list)):
+            if isinstance(my_list[j], int):
+                if my_list[j] > max_val:
+                    max_val = my_list[j]
+    return max_val
