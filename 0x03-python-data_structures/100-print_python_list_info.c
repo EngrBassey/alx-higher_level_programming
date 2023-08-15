eef1439ba4c95a4c3319e0b18e81707aeb77b320
EngrBassey <basseyleads001>
2023-08-15 14:23:54 -0700

    writting basic info in Cpython

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,26 @@
+#include <Python>
+
+/**
+  * print_python_list_info - function that print some basic infp about python
+  * @p: Objects
+  */
+
+void print_python_list_info(PyObject *p)
+{
+	int size, ptr, i;
+	PyObject *obj;
+
+	size = Py_size(p);
+	ptr = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", ptr);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+}
