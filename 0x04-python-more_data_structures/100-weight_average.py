e5a306406cfe1bb2fc6d8379319858de70bed8f7
EngrBassey <basseyleads001>
2023-08-17 15:16:41 -0700

    function that returns the weighted average of all integers tuple

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    my_pd = 0
+    my_adds = 0
+
+    for i, j in my_list:
+        my_pd += i * j
+        my_adds += j
+    return (my_pd / my_adds)
