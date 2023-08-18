5f6a56b1103bf950ef95076198e629ed07714904
EngrBassey <basseyleads001>
2023-08-18 09:12:43 -0700

    Romans figure

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dec = 0
-    i = 0
-    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-                 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90,
-                 'CD': 400, 'CM': 900}
-    while i < len(roman_string):
-        if i + 1 < len(roman_string) and roman_string[i:1 + 2] in roman_map:
-            dec += roman_map[roman_string[i:1 + 2]]
-            i += 2
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = roman_dict[char]
+        if value >= prev_value:
+            total += value
         else:
-            dec += roman_map[roman_string[i]]
-            i += 1
-    return (dec)
+            total -= value
+        prev_value = value
+
+    return total
