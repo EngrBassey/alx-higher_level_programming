766ddcc3015a81440a4dcfa63ee547d2dcfebefa
EngrBassey <basseyleads001@gmail.com>
2023-10-05 09:25:00 -0700

    function that adds 2 integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+""" add Funn """
+def add_integer(a, b=98):
+    """ functions that add """
+    if not (isinstance(a, float) or isinstance(a, int)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, float) or isinstance(b, int)):
+        raise TypeError("b must be an integer")
+
+    c = int(a)
+    d = int(b)
+
+    return c + d
