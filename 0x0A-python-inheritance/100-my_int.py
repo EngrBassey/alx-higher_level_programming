7e0f07eec6a07608c69ed2ccc86d55e2eaf2d680
EngrBassey <basseyleads001@gmail.com>
2023-10-11 06:52:13 -0700

    class MyInt that inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class MyInt that inherits from int"""
+
+
+class MyInt(int):
+    """function that interchange operators"""
+    def __eq__(self, other):
+        return super().__ne__(other)
+    
+    def __ne__(self, other):
+        return super().__eq__(other)
