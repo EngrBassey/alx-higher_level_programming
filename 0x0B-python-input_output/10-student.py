f72cbf9ae1e1a2c2c5572b749b89d6a087e9d28b
EngrBassey <basseyleads001@gmail.com>
2023-10-11 12:40:42 -0700

    class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' Module for Student class '''
+
+
+class Student:
+    ''' Representaton of a Student '''
+
+    def __init__(self, first_name, last_name, age):
+        ''' Instantiation '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        ''' Retrieves dictionary representation '''
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__.copy()
