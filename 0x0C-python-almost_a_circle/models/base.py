81fca141e418b378401229b0b7d2422c2250d002
EngrBassey <basseyleads001@gmail.com>
2023-10-17 04:49:25 -0700

    creating json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+import json
 """"Model to create a base class"""
 
 
@@ -14,3 +14,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """"loading json to dic rep"""
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
