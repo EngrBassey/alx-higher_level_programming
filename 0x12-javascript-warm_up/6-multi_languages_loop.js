051d0f19495fcd0f4baac9e512e9a64b57adcfbe
Engr Bassey <basseyleads001@gmail.com>
2023-12-11 14:52:52 -0800

    script that prints x times “C is fun”

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-
 const items = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < items.length; i++) {
+for (const i = 0; i < items.length; i++) {
   console.log(items[i]);
 }
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+let args = process.argv;
+
+if (args.length === 2) {
+  console.log('Missing number of occurrences');
+}
+for (let i = 1; i <= parseInt(args[2]); i++) {
+  console.log('C is fun');
+}
