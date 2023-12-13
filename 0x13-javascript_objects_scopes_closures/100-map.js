97771f5c283caaa59445eb54d5bc04a4cda5bd99
Engr Bassey <basseyleads001@gmail.com>
2023-12-13 10:21:52 -0800

    script that imports an array and computes a new array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,7 +3,8 @@
 const list = require('./100-data').list;
 
 const modifiyArr = list.map(function (element, index) {
-  return (element * index);
+  return element * index;
 });
+
 console.log(list);
 console.log(modifiyArr);
