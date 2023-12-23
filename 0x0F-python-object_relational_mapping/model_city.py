ee4c31dd6fb3915039f6864a237e949378e247a4
EngrBassey <basseyleads001@gmail.com>
2023-12-22 20:32:21 -0800

    Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """
-module that contains class definiton
+Contains the class definition of a City
 """
-
-from sqlalchemy import Column, String, Integer, MetaData
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
 
-
-class State(Base):
+class City(Base):
     """
-    funtion inherent from Base
+    Class that defines each city
     """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
