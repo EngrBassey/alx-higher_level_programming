0b7370462300a5c6fd1658c92a261f0bfccfbdb6
EngrBassey <basseyleads001@gmail.com>
2024-01-05 22:22:31 +0000

    Display status code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 #Display only body of 200 status code
-curl -sLI $1 -o /dev/null -w '%{http_code}\n' | grep -q 200 && curl -sL $1
+# Use curl to send a GET request and display "Route 2" if the status code is 200
+curl -s -o /dev/null -w "%{http_code}" "$1" | {
+    read status
+    if [ "$status" -eq 200 ]; then
+        # Output "Route 2"
+        echo "Route 2"
+    fi
+}
