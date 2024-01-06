b5569790c6924acb9a58efd8e8cbdf1ebe252b4f
EngrBassey <basseyleads001@gmail.com>
2024-01-06 07:07:55 -0800

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,13 @@
 """Module to display body response"""
 
 if __name__ == "__main__":
-import urllib.request
+    import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    body_res = response.read()
-    utf_8_res = body_res.decode('utf-8')
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body_res = response.read()
+        utf_8_res = body_res.decode('utf-8')
 
-    print("Body response:")
-    print("    - type:", type(body_res))
-    print("    - content:", body_res)
-    print("    - utf8 content:", utf_8_res)
+        print("Body response:")
+        print("    - type:", type(body_res))
+        print("    - content:", body_res)
+        print("    - utf8 content:", utf_8_res)
